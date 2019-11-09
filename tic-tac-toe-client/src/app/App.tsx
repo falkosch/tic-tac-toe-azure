53df57fb0f6b226f725343756606cc818d8a0f97
Falko Schwabe <falkosch@hotmail.de>
2019-11-09 07:32:54 +0100

    add metamodel for a generalized tic tac toe game

--- tic-tac-toe-client/src/app/App.tsx
+++ tic-tac-toe-client/src/app/App.tsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
+import GameView from './game-view/GameView';
 
 import './App.css';
 
 import logo from './logo.svg';
 
-const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Tic tac toe</p>
-    </header>
-  </div>
-);
+import MockDefender from '../defender/MockDefender';
+import { Game } from '../meta-model/Game';
+
+const defender = new MockDefender();
+
+let game: Game;
+async function handshake(): Promise<Game> {
+  game = await defender.handshake();
+  return game;
+}
+
+const App: React.FC = () => {
+  const [_game, setGame] = useState(game);
+
+  async function newGame(): Promise<void> {
+    setGame(await handshake());
+  }
+
+  return (
+    <div className="app">
+      <Navbar expand="lg" className="bg-light">
+        <Navbar.Brand href="/">
+          <img src={logo} width="30" height="30" className="App-logo" alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Button onClick={newGame}>New game</Button>
+        </Navbar.Collapse>
+      </Navbar>
+      <GameView game={_game} />
+    </div>
+  );
+};
 
 export default App;
---
+++ tic-tac-toe-client/src/defender/MockDefender.ts
@@ -0,0 +1,31 @@
+// @ts-ignore
+import fpTimes from 'lodash/fp/times';
+
+import { NoCellOwner } from '../meta-model/Cell';
+import { Defender } from '../meta-model/Defender';
+import { Game } from '../meta-model/Game';
+import { GameAction } from '../meta-model/GameAction';
+import { GameReaction } from '../meta-model/GameReaction';
+
+const DefaultHeight = 3;
+const DefaultWidth = 3;
+
+export default class MockDefender implements Defender {
+  handshake(): Promise<Game> {
+    return Promise.resolve({
+      board: {
+        cells: fpTimes(() => NoCellOwner)(DefaultHeight * DefaultWidth),
+        height: DefaultHeight,
+        width: DefaultWidth,
+      },
+      consecutiveness: [],
+    });
+  }
+
+  defend(gameAction: GameAction): Promise<GameReaction> {
+    return Promise.resolve({
+      board: gameAction.board,
+      consecutiveness: [],
+    });
+  }
+}
