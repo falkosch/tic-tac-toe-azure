aa98c668c19d8b13a8abc1a36d9df13c53f4f3f6
Falko Schwabe <falkosch@hotmail.de>
2019-11-11 19:51:43 +0100

    client defender can handle attack actions (although it is still a passthrough for now)

--- tic-tac-toe-client/src/defender/ClientDefender.ts
+++ tic-tac-toe-client/src/defender/ClientDefender.ts
@@ -1,13 +1,16 @@
 import fpTimes from 'lodash/fp/times';
 
+import { AttackAction, GameAction } from '../meta-model/GameAction';
+import { Board } from '../meta-model/Board';
 import { CellOwner } from '../meta-model/CellOwner';
 import { Defender } from '../meta-model/Defender';
 import { Game } from '../meta-model/Game';
-import { GameAction } from '../meta-model/GameAction';
 import { GameReaction } from '../meta-model/GameReaction';
 
 const DefaultHeight = 3;
 const DefaultWidth = 3;
+
+type GameReactionModifier = (gameReaction: GameReaction) => GameReaction;
 
 export class ClientDefender implements Defender {
   handshake(): Promise<Game> {
@@ -23,7 +26,23 @@ export class ClientDefender implements Defender {
     });
   }
 
-  defend(__gameAction: GameAction): Promise<GameReaction> {
-    return Promise.reject(new Error('not implemented'));
+  defend(gameAction: GameAction): Promise<GameReaction> {
+    let gameReaction: GameReaction = {
+      board: gameAction.board,
+      consecutiveness: [],
+      endedReaction: undefined,
+    };
+    if (gameAction.attack) {
+      gameReaction = this.handleAttack(gameAction.board, gameAction.attack)(gameReaction);
+    }
+    return Promise.resolve(gameReaction);
+  }
+
+  private handleAttack(board: Board, __attack: AttackAction): GameReactionModifier {
+    return (gameReaction: GameReaction) => ({
+      board,
+      consecutiveness: gameReaction.consecutiveness,
+      endedReaction: gameReaction.endedReaction,
+    });
   }
 }
