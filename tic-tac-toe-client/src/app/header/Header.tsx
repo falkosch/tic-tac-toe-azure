1611e3f5e4cd2b205e4b8528afb989a80c7ddec7
Falko Schwabe <falko.schwabe@gmail.com>
2020-01-01 08:49:21 +0100

    fix responsiveness issues and typos

--- tic-tac-toe-client/src/app/header/Header.tsx
+++ tic-tac-toe-client/src/app/header/Header.tsx
@@ -7,7 +7,7 @@ import styles from './Header.module.scss';
 
 export const Header: FC<{}> = ({ children }) => (
   <div className={styles.header}>
-    <Navbar sticky="top" expand="md" bg="light" variant="light">
+    <Navbar fixed="top" expand="md" bg="light" variant="light">
       <Navbar.Brand href="https://github.com/falkosch/tic-tac-toe-azure">
         <img className={`${styles.logo} d-inline-block align-top`} src={logo} alt="logo" />
         <span> Tic Tac Toe Game</span>
--- tic-tac-toe-client/src/app/winner-view/WinnerView.tsx
+++ tic-tac-toe-client/src/app/winner-view/WinnerView.tsx
@@ -44,7 +44,7 @@ export const WinnerView: FC<{
     <div className={styles.winner}>
       {
         representsDraw(winner) && (
-          'It&apos;s a draw!'
+          <>It&apos;s a draw!</>
         )
       }
       {
