49d25d899cba5425866e7b16833e8eb2fbf456ea
Falko Schwabe <falko.schwabe@gmail.com>
2020-01-01 01:19:42 +0100

    support persistence via IndexedDB store, fallback to localStorage, or fallback to transient in-memory map

--- tic-tac-toe-client/src/computer-players/ai-agent/StorableAgent.ts
+++ tic-tac-toe-client/src/computer-players/ai-agent/StorableAgent.ts
@@ -9,47 +9,182 @@ export interface StorableAgent<BrainType extends BrainStatistics> {
   brain: BrainType;
 }
 
-/**
- * NOTE: The function is made asynchronous although {@code localStorage.setItem} isn't asynchronous.
- * It is for preparing supporting different storage targets.
- * @param id a unique identifier
- * @param version the version of the object format
- * @param brain the object to persist
- */
+interface Persister {
+  store<BrainType extends BrainStatistics>(
+    id: string,
+    data: Readonly<StorableAgent<BrainType>>,
+  ): Promise<void>;
+  load<BrainType extends BrainStatistics>(
+    id: string,
+  ): Promise<StorableAgent<BrainType> | undefined>;
+}
+
+interface DatabaseInitializationCallback {
+  (database?: Readonly<IDBDatabase>, error?: Readonly<Error>): void;
+}
+
+const indexedDBConfiguration = {
+  database: 'tic-tac-toe-agents',
+  objectStore: 'agents',
+  version: 1,
+};
+
+const persisterInitialization = new Promise<Persister>((resolve) => {
+  initializeDatabase(
+    (database, error) => {
+      if (database === undefined || error) {
+        if (localStorageIsAccessible()) {
+          resolve(createLocalStoragePersister());
+        } else {
+          resolve(createTransientInMemoryPersister());
+        }
+      } else {
+        resolve(createIndexedDBPersister(database));
+      }
+    },
+  );
+});
+
+function createTransaction(
+  database: Readonly<IDBDatabase>,
+  readOnly: boolean,
+  transactionResolve: Function,
+  transactionReject: Function,
+): IDBTransaction {
+  const dbTransaction = database.transaction(
+    [indexedDBConfiguration.objectStore],
+    readOnly ? 'readonly' : 'readwrite',
+  );
+
+  dbTransaction.oncomplete = () => {
+    transactionResolve();
+  };
+
+  dbTransaction.onerror = () => {
+    transactionReject(new Error(dbTransaction.error.message));
+  };
+
+  return dbTransaction;
+}
+
+function createIndexedDBPersister(database: Readonly<IDBDatabase>): Persister {
+  return {
+    store(id, data) {
+      return new Promise((storeResolve, storeReject) => {
+        createTransaction(database, false, storeResolve, storeReject)
+          .objectStore(indexedDBConfiguration.objectStore)
+          .put(data, id);
+      });
+    },
+    async load(__id) {
+      return new Promise((loadResolve, loadReject) => {
+        const request = createTransaction(database, false, () => {}, loadReject)
+          .objectStore(indexedDBConfiguration.objectStore)
+          .get(__id);
+
+        request.onsuccess = () => {
+          loadResolve(request.result);
+        };
+
+        request.onerror = () => {
+          loadReject(new Error(request.error?.message));
+        };
+      });
+    },
+  };
+}
+
+function initializeDatabase(callback: DatabaseInitializationCallback): void {
+  if (!window.indexedDB || !window.indexedDB.open) {
+    callback(undefined, new Error('indexedDB not supported'));
+    return;
+  }
+
+  const dbOpenRequest = window.indexedDB.open(
+    indexedDBConfiguration.database,
+    indexedDBConfiguration.version,
+  );
+
+  if (!dbOpenRequest) {
+    callback(undefined, new Error('failed to open indexedDB database'));
+    return;
+  }
+
+  dbOpenRequest.onerror = () => {
+    callback(undefined, new Error(dbOpenRequest.error?.message));
+  };
+
+  dbOpenRequest.onupgradeneeded = (event) => {
+    if (event.oldVersion < 1) {
+      dbOpenRequest.result.createObjectStore(indexedDBConfiguration.objectStore);
+    }
+  };
+
+  dbOpenRequest.onsuccess = () => {
+    callback(dbOpenRequest.result);
+  };
+}
+
+function localStorageIsAccessible(): boolean {
+  const dummyKey = 'dummy';
+  try {
+    localStorage.setItem(dummyKey, dummyKey);
+    localStorage.removeItem(dummyKey);
+  } catch {
+    return false;
+  }
+  return true;
+}
+
+function createLocalStoragePersister(): Persister {
+  return {
+    async store(id, data) {
+      const jsonData = JSON.stringify(data);
+      localStorage.setItem(id, jsonData);
+    },
+    async load(id) {
+      const stored = localStorage.getItem(id);
+      if (!stored) {
+        return undefined;
+      }
+      return JSON.parse(stored);
+    },
+  };
+}
+
+function createTransientInMemoryPersister(): Persister {
+  const memory: Record<string, StorableAgent<any>> = {};
+  return {
+    async store(id, data) {
+      memory[id] = data;
+    },
+    async load(id) {
+      return memory[id];
+    },
+  };
+}
+
 export async function persistAgent<BrainType extends BrainStatistics>(
   id: string,
   version: number,
   brain: Readonly<BrainType>,
 ): Promise<void> {
-  const data: StorableAgent<BrainType> = {
+  const persister = await persisterInitialization;
+  return persister.store<BrainType>(id, {
     version,
     brain,
-  };
-  localStorage.setItem(id, JSON.stringify(data));
+  });
 }
 
-/**
- * NOTE: The function is made asynchronous although {@code localStorage.getItem} isn't asynchronous.
- * It is for preparing supporting different storage targets.
- * @param id a unique identifier
- * @param version the version of the object format
- * @param defaultBrain a default value to return when no object for the given id exists yet
- */
 export async function loadAgent<BrainType extends BrainStatistics>(
   id: string,
   version: number,
-  defaultBrain?: BrainType,
+  defaultBrain?: Readonly<BrainType>,
 ): Promise<BrainType | undefined> {
-  const stored = localStorage.getItem(id);
-  if (stored) {
-    try {
-      const parsed: StorableAgent<BrainType> = JSON.parse(stored);
-      if (parsed && parsed.version === version) {
-        return parsed.brain;
-      }
-    } catch (e) {
-      console.error('Could not load agent data for ', id, ' due to ', e);
-    }
+  const persister = await persisterInitialization;
+  const stored = await persister.load<BrainType>(id);
+  if (stored && stored.version === version && stored.brain) {
+    return stored.brain;
   }
   return defaultBrain;
 }
