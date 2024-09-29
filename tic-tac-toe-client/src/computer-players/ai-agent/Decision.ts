9f0b0e55fbce8be38812a163ae7adae35d35536c
Falko Schwabe <2913295+falkosch@users.noreply.github.com>
2024-09-29 18:37:49 +0200

    AI agents adhere the same cell action behavior as it is for a human player, i.e. choosing an invalid action results in skip, the board is not modified, and the opponent gets the turn immediately

--- tic-tac-toe-client/src/computer-players/ai-agent/Decision.ts
+++ tic-tac-toe-client/src/computer-players/ai-agent/Decision.ts
@@ -4,21 +4,6 @@ export interface Decision {
   cellsAtToAttack: ReadonlyArray<number>;
 }
 
-export function validateDecision(
-  cells: ReadonlyArray<CellOwner>,
-  decision: Readonly<Decision>,
-): boolean {
-  return decision.cellsAtToAttack.reduce<boolean>(
-    (validAcc, cellAt) => {
-      if (!validAcc) {
-        return false;
-      }
-      return cells[cellAt] === CellOwner.None;
-    },
-    true,
-  );
-}
-
 export function findFreeCellIndices(cells: ReadonlyArray<CellOwner>): number[] {
   const freeCellIndices: number[] = [];
   cells.forEach((cellOwner, index) => {
