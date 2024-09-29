c097987defc409db1981cd9bd05a5a3243e8ac65
Falko Schwabe <2913295+falkosch@users.noreply.github.com>
2024-09-29 18:37:19 +0200

    make the defender selectable and provide a client-side defender impl and the actual azure function defender impl

--- tic-tac-toe-client/src/defender/ClientDefender.ts
+++ tic-tac-toe-client/src/defender/ClientDefender.ts
@@ -1,31 +1,29 @@
+import fpTimes from 'lodash/fp/times';
+
 import { CellOwner } from '../meta-model/CellOwner';
 import { Defender } from '../meta-model/Defender';
 import { Game } from '../meta-model/Game';
 import { GameAction } from '../meta-model/GameAction';
 import { GameReaction } from '../meta-model/GameReaction';
 
-export class MockDefender implements Defender {
+const DefaultHeight = 3;
+const DefaultWidth = 3;
+
+export class ClientDefender implements Defender {
   handshake(): Promise<Game> {
     return Promise.resolve({
       board: {
-        cells: [
-          CellOwner.X, CellOwner.O, CellOwner.X,
-          CellOwner.O, CellOwner.X, CellOwner.O,
-          CellOwner.O, CellOwner.X, CellOwner.O,
-        ],
+        cells: fpTimes<CellOwner>(() => CellOwner.None)(DefaultWidth * DefaultHeight),
         dimensions: {
-          height: 3,
-          width: 3,
+          height: DefaultHeight,
+          width: DefaultWidth,
         },
       },
       consecutiveness: [],
     });
   }
 
-  defend(gameAction: GameAction): Promise<GameReaction> {
-    return Promise.resolve({
-      board: gameAction.board,
-      consecutiveness: [],
-    });
+  defend(__gameAction: GameAction): Promise<GameReaction> {
+    return Promise.reject(new Error('not implemented'));
   }
 }
