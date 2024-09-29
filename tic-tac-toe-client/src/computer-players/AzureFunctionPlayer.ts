9d816ab05806ff99d5cdeadd383d87fc0a24fc5e
Falko Schwabe <2913295+falkosch@users.noreply.github.com>
2024-09-29 18:37:31 +0200

    implement azure function player on the client

--- tic-tac-toe-client/src/computer-players/AzureFunctionPlayer.ts
+++ tic-tac-toe-client/src/computer-players/AzureFunctionPlayer.ts
@@ -1,13 +1,27 @@
+import axios from 'axios';
+
 import { AttackGameAction } from '../meta-model/GameAction';
 import { Player } from '../meta-model/Player';
 import { PlayerTurn } from '../meta-model/PlayerTurn';
 
 /**
- * TODO Preliminary, actual implementation will call Azure Function at
- * fstictactoegame.azurewebsites.net
+ * Posts the current game state to the Azure Function fstictactoegame. The function can analyse
+ * the sent game state like the other AI player implementations would do. It can so decide for a
+ * a valuable action and return it as response.
  */
 export class AzureFunctionPlayer implements Player {
-  takeTurn(__playerTurn: Readonly<PlayerTurn>): Promise<AttackGameAction> {
-    throw new Error('not implemented yet');
+  private axiosInstance = axios.create({
+    // baseURL: 'http://localhost:7071',
+    baseURL: 'https://fstictactoegame.azurewebsites.net',
+  });
+
+  async takeTurn(playerTurn: Readonly<PlayerTurn>): Promise<AttackGameAction> {
+    try {
+      return (await this.axiosInstance.post('/api/takeTurn', playerTurn)).data;
+    } catch (e) {
+      console.error('Azure player encountered error', e);
+      // errors result in skip actions for now
+      return { affectedCellsAt: [] };
+    }
   }
 }
