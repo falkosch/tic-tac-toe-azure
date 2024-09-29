72240e1ba4cb08b50609ec5110072753a390a6c4
Falko Schwabe <2913295+falkosch@users.noreply.github.com>
2024-09-29 18:37:19 +0200

    clean up code

--- tic-tac-toe-client/src/mechanics/MapCellOwnerToImage.ts
+++ tic-tac-toe-client/src/mechanics/MapCellOwnerToImage.ts
@@ -3,10 +3,11 @@ import { CellOwner } from '../meta-model/CellOwner';
 import strokeO from './assets/stroke-o.svg';
 import strokeX from './assets/stroke-x.svg';
 
-export function mapCellOwnerToImage(cellOwner: CellOwner): string {
-  const cellOwnersToImages = {
-    [CellOwner.O]: strokeO,
-    [CellOwner.X]: strokeX,
-  };
+const cellOwnersToImages = {
+  [CellOwner.O]: strokeO,
+  [CellOwner.X]: strokeX,
+};
+
+export function mapCellOwnerToImage(cellOwner: CellOwner): string | undefined {
   return cellOwnersToImages[cellOwner];
 }
